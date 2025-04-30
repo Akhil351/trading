6a37e673565b0545e61cd8ef66fd0359d1d9a6ce
Akhileswar <115480942+Akhil351@users.noreply.github.com>
2025-04-30 16:00:56 +0530

    Update Store.js

--- trading-frontend/src/state/Store.js
+++ trading-frontend/src/state/Store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
-import authReducer from "./Reducer";
+import { authReducer } from "./Reducer";
 
 
 
